34d460b1c62b2d9c1f4b8239a2f2694807bb2ca8
deepakmehta <61088464+Deepak06mehta@users.noreply.github.com>
2022-09-16 21:53:46 +0530

    BootStrap changes
    BootStrap changes

--- MangoResturant/Mango.Web/Services/IServices/IBaseService.cs
+++ MangoResturant/Mango.Web/Services/IServices/IBaseService.cs
@@ -4,6 +4,7 @@ namespace Mango.Web.Services.IServices
 {
     public interface IBaseService: IDisposable
     {
+        //interface can have methods,properties,indexers,events as its members but only contains the definitions of these.
         ResponseDTO responseModel { get; set; }
         Task<T> SendAsync<T>(ApiRequest apiRequest); 
     }
